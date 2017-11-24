2f5f842ffc8acb99c0c3dc5e8b46e4782afdb5d2
NotHavoc <33915989+NotHavoc@users.noreply.github.com>
2017-11-24 10:25:56 -0500

    Update mycog.py

--- mycog/mycog.py
+++ mycog/mycog.py
@@ -21,35 +21,3 @@ class Mycog:
 def setup(bot):
     bot.add_cog(Mycog(bot))
     
-class Wednesday:
-    #Cog to notify the server when it is wednesday
-    def __init__(self, bot):
-        self.bot = bot
-        #self.triggered = false 
-        
-    async def on_message(self, message):
-        #reads server messages, if it is the first message of wednesday posts the notification
-        channel = message.channel
-        author = message.author
-       # weekday = datetime.now().isoweekday()
-        weekday = 3
-        if message.server is None:
-            return
-        if author == self.bot.user:
-            return
-        
-        if not self.bot.user_allowed(message):
-            return
-        if self.is_command(message):
-            return
-        
-        if (weekday != 3):
-        #    self.tiggered = false
-            return
-        #if (triggered == true):
-         #   return
-      
-        await self.bot.say("It is Wednesday my dudes")
-        triggered = true
- def setup(bot):
-     bot.add_cog(Wednesday(bot))    
