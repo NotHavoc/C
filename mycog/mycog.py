3406b5991c35c902faf7589dbe81115d885830c0
NotHavoc <33915989+NotHavoc@users.noreply.github.com>
2017-11-24 11:29:38 -0500

    Update mycog.py

--- mycog/mycog.py
+++ mycog/mycog.py
@@ -5,7 +5,7 @@ from discord.ext import commands
 from cogs.utils import checks
 from cogs.utils.dataIO import dataIO
 from cogs.utils.chat_formatting import box, pagify, escape_mass_mentions
-from random import choice
+
 
 
 class Mycog:
