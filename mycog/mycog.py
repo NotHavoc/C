32fc886c8068b56296e2541a25526bcb466541f5
NotHavoc <33915989+NotHavoc@users.noreply.github.com>
2017-11-24 11:37:19 -0500

    Update mycog.py

--- mycog/mycog.py
+++ mycog/mycog.py
@@ -1,11 +1,12 @@
 import os
 import discord
 import datetime
+import copy
 from discord.ext import commands
-#from cogs.utils import checks
-#from cogs.utils.dataIO import dataIO
-#from cogs.utils.chat_formatting import box, pagify, escape_mass_mentions
-
+from cogs.utils import checks
+from cogs.utils.dataIO import dataIO
+from cogs.utils.chat_formatting import box, pagify, escape_mass_mentions
+from random import choice
 
 
 class Mycog:
@@ -22,7 +23,42 @@ class Mycog:
         await self.bot.say("I can do stuff!")
         day = datetime.datetime.now().isoweekday()
         await self.bot.say(day)
-         
+            
+    def is_command(self, msg):
+        if callable(self.bot.command_prefix):
+            prefixes = self.bot.command_prefix(self.bot, msg)
+        else:
+            prefixes = self.bot.command_prefix
+        for p in prefixes:
+            if msg.content.startswith(p):
+                return True
+        return False
+
+        
+    async def on_message(self, message):
+        #reads server messages, if it is the first message of wednesday posts the notification
+        channel = message.channel
+        author = message.author
+       # weekday = datetime.datetime.now().isoweekday()
+        weekday = 3
+        if message.server is None:
+            return
+        if author == self.bot.user:
+            return
+        
+        if not self.bot.user_allowed(message):
+            return
+        #if self.is_command(message):
+        #    return
+        
+        if (weekday != 3):
+        #    self.tiggered = false
+            return
+        #if (triggered == true):
+         #   return
+      
+        await self.bot.say("It is Wednesday my dudes")
+        triggered = true    
 
 def setup(bot):
     bot.add_cog(Mycog(bot))
