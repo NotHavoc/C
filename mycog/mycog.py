cc556b43ff384060a607537b3b7fea0200087f1f
NotHavoc <33915989+NotHavoc@users.noreply.github.com>
2017-11-24 11:27:05 -0500

    Update mycog.py

--- mycog/mycog.py
+++ mycog/mycog.py
@@ -23,42 +23,7 @@ class Mycog:
         await self.bot.say("I can do stuff!")
         day = datetime.datetime.now().isoweekday()
         await self.bot.say(day)
-            """
-    def is_command(self, message):
-        if callable(self.bot.command_prefix):
-            prefixes = self.bot.command_prefix(self.bot, msg)
-        else:
-            prefixes = self.bot.command_prefix
-        for p in prefixes:
-            if msg.content.startswith(p):
-                return True
-        return False
-"""
-    async def on_message(self, message):
-        #reads server messages, if it is the first message of wednesday posts the notification
-        channel = message.channel
-        author = message.author
-       # weekday = datetime.datetime.now().isoweekday()
-        weekday = 3
-        if message.server is None:
-            return
-        if author == self.bot.user:
-            return
-        
-        if not self.bot.user_allowed(message):
-            return
-        #if self.is_command(message):
-        #    return
-        
-        if (weekday != 3):
-        #    self.tiggered = false
-            return
-        #if (triggered == true):
-         #   return
-      
-        yield from self.bot.say("It is Wednesday my dudes")
-        triggered = true    
+         
 
 def setup(bot):
-    n = Mycog(bot)
-    bot.add_cog(n)
+    bot.add_cog(Mycog(bot))
